8f16e70f0786f48f78db2baca3b4e5ee26a482bd
Jonatan449 <jonatanespindola808@gmail.com>
2026-08-08 23:41:12 -0300

    Create 04_if_else.py

---
+++ Jonatan449/04_if_else.py
@@ -0,0 +1,8 @@
+ano = int(input('Em que ano você nasceu? '))
+
+idade = 2030 - ano
+
+if idade >= 18:
+    print(f'Em 2030 você será maior de idade e terá {idade}.')
+else:
+    print(f'Em 2030 você será menor de idade e vai ter {idade}')
