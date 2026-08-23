6ca464081d71f1e397c279db7c06d984344fe245
Jonatan449 <jonatanespindola808@gmail.com>
2026-08-23 14:59:32 -0300

    Atualizar o cadastro_alunos.py
    só coloquei o arquivo certo

--- Jonatan449/cadastro_alunos.py
+++ Jonatan449/cadastro_alunos.py
@@ -1,1 +1,28 @@
-feat: cria protótipo de cadastro de alunos
+cores={"amarelo": "\033[33m",
+       "vermelho": "\033[31m",
+       "branco": "\033[1;97m",
+       "azul": "\033[34m",
+       "ciano": "\033[36m",
+       "roxo": "\033[35m",
+       "cinza": "\033[37m",
+       "verde": "\033[32m",
+       "limpa": "\033[m"
+}
+alunos=[]
+temp={}
+quant=int(input("Quantos alunos quer cadastar?: "))
+aluno=0
+for _ in range(quant):
+       aluno+=1
+       print(f'{cores["amarelo"]}-={cores["limpa"]}'*15)
+       print(f'{cores["branco"]}Aluno{aluno}:{cores["limpa"]}', end='')
+       print("|")
+       temp["nome"]=input("Nome do aluno: ")
+       temp["media"]=float(input("Média desse aluno: "))
+       alunos.append(temp.copy())
+print(f"{cores["branco"]}_{cores["limpa"]}"*20)
+for a in alunos:
+       if a["media"]>=7:
+          print(f"{a["nome"]} foi {cores["verde"]}APROVADO{cores["limpa"]}")
+       else:
+          print(f"{a["nome"]} foi {cores["vermelho"]}REPROVADO{cores["limpa"]}")
